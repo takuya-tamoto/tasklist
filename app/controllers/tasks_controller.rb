3fab3de3a0583b67f34abdaa1e86000c44b7d1ca
takuya.tamoto <taku92860110@gmail.com>
2020-02-25 04:08:32 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -61,5 +61,5 @@ def set_task
 end
 
 def task_params
-  params.require(:task).permit(:content)
+  params.require(:task).permit(:content, :status)
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  validates :content, presence: true, length: { maximum: 255 }
+  validates :status, presence: true, length: { maximum: 10 ,message: '10文字以内です'}
 end
