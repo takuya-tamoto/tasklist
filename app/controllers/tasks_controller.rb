6db57a21de56b1c7fc4d41e22427d916ac842726
takuya.tamoto <taku92860110@gmail.com>
2020-03-02 12:42:23 +0000

    kadaikanryou

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,18 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   #同じものをまとめる
   
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
   
   def show
-    #set_task
-    #@task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   def new
@@ -17,25 +20,23 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = 'タスクが作成されました。'
-      redirect_to @task
+      redirect_to root_url
     else
+      @task = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'タスクが作成されません。'
       render :new
     end
   end
   
   def edit
-    #set_task
-    #@task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
   
   def update
-    #set_task
-    #@task = Task.find(params[:id])
+    @task = Task.find(params[:id])
 
   if @task.update(task_params)
     flash[:success] = 'タスクが編集されました'
@@ -47,21 +48,23 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    #set_task
-    #@task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'タスクは削除されました'
     redirect_to tasks_url
   end
-end
 
-private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  private
 
-def task_params
-  params.require(:task).permit(:content, :status)
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
