7a420ca0c0247d1e45508110c3f193863a63a8bd
takuya.tamoto <taku92860110@gmail.com>
2020-03-02 07:35:25 +0000

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = '登録完了'
+      redirect_to root_url
+    else
+      flash.now[:danger]= '登録失敗'
+      render :new
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
